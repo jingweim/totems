d6f452a51d90e97fe131f0a70da8e8b4058af9f7
Jingwei Ma <jingweima.1998@gmail.com>
2022-10-24 17:46:15 -0700

    Final touches

--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,11 @@
 window.HELP_IMPROVE_VIDEOJS = false;
 
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+} 
+
 var INTERP_BASE = "https://homes.cs.washington.edu/~kpar/nerfies/interpolation/stacked";
 var NUM_INTERP_FRAMES = 240;
 
@@ -21,6 +27,21 @@ function setInterpolationImage(i) {
 
 
 $(document).ready(function() {
+
+    // Get the button:
+    let mybutton = document.getElementById("myBtn");
+
+    function scrollFunction() {
+      if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+      } else {
+        mybutton.style.display = "none";
+      }
+    }
+
+    // When the user scrolls down 20px from the top of the document, show the button
+    window.onscroll = function() {scrollFunction()};
+
     // Check for click events on the navbar burger icon
     $(".navbar-burger").click(function() {
       // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
@@ -76,3 +97,5 @@ $(document).ready(function() {
     bulmaSlider.attach();
 
 })
+
+
